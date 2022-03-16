05c7654c111a07616c0c7b0a12aa6ba8451a3c48
LucioDangelo <ldangelo@itba.edu.ar>
2022-03-16 04:29:55 -0300

    Primer intento de incorportar React

---
+++ proyecto-react/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GenresInDb from './GenresInDb';
+function App() {
+  return (
+    <React.Fragment>
+          <GenresInDb />
+          <GenresInDb />
+          <GenresInDb />
+    </React.Fragment>
+  );
+}
+
+export default App;
--- proyecto-react/src/index.js
+++ proyecto-react/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
  ReactDOM.render(
@@ -11,16 +11,7 @@ import reportWebVitals from './reportWebVitals';
    document.getElementById('root')
  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
 
-// function home(){
-//   return (
-//       <section className="banner">
-//           <h1 className="titulo">Tenemos lo mejor para ti...</h1>
-//       </section>
-//   )
-// }
-// export default home;
+
