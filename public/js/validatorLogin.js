9b36a583bde55546dfd61abe71d8c8b407f57e7c
DarioValer <dario-valeriano@hotmail.com>
2022-03-24 08:14:59 -0300

    productDetail, caja productos, etc

--- public/js/validatorLogin.js
+++ public/js/validatorLogin.js
@@ -1,20 +1,26 @@
  /*window.addEventListener('load', function(){*/
- /*const formLogin = document.querySelector('form.form-login');
+ const formLogin = document.querySelector('form.form-login');
 
  /* LOGIN */
 
- /*formLogin.addEventListener('submit', function(event){
+ formLogin.addEventListener('submit', function(event){
 
      let errors = [];
 
      let inputEmail = document.querySelector('#email');
+     let errorEmail = document.getElementById('errorEmail')
  if (inputEmail.value == '') {
-     errors.push('Email es un Campo obligatorio')
+    // errors.push('Email es un Campo obligatorio')
+    errorEmail.innerHTML = '';
+    errorEmail.innerHTML = 'Email es un campooo obligatorio'
  } 
 
  let inputPassword = document.querySelector('#password');
+ let errorPassword = document.getElementById('errorPassword');
  if (inputPassword.value == '') {
-     errors.push('Contrasena es un Campo obligatorio')
+     //errors.push('Contrasena es un Campo obligatorio')
+     errorPassword.innerHTML = '';
+     errorPassword.innerHTML = "Contraseña es un campo obligatorio"
  }
 
  if(errors.length > 0){
